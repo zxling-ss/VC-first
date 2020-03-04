364c7752b2dd10a32070d6e42be2886a44761298
zxling-ss <57402329+zxling-ss@users.noreply.github.com>
2020-03-04 21:38:31 +0800

    Add files via upload

---
+++ MFCApplication3/MFCApplication3View.cpp
@@ -0,0 +1,119 @@
+﻿
+// MFCApplication3View.cpp: CMFCApplication3View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication3.h"
+#endif
+
+#include "MFCApplication3Doc.h"
+#include "MFCApplication3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication3View
+
+IMPLEMENT_DYNCREATE(CMFCApplication3View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication3View, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCApplication3View 构造/析构
+
+CMFCApplication3View::CMFCApplication3View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCApplication3View::~CMFCApplication3View()
+{
+}
+
+BOOL CMFCApplication3View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication3View 绘图
+void CMFCApplication3View::OnLButtonDown(UINT  nFlags , CPoint point)
+{
+	CMFCApplication3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	pDoc->sum += 1;
+	CString m;
+	CClientDC dc(this);
+	m.Format(_T("%d"), pDoc->sum);
+	dc.TextOutW(550, 550, m);
+	CView::OnLButtonDown(nFlags, point);
+}
+
+void CMFCApplication3View::OnDraw(CDC* pDC)
+{
+	CMFCApplication3Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	CString	s = _T("我是***");
+	pDC->TextOutW(200, 200, s);
+	int a = 0;
+	CString b;
+	b.Format(_T("%d"), a);
+	pDC->TextOutW(250, 250, b);
+	/*pDC->TextOutW(400, 400, s);
+	pDC->TextOutW(500, 500, C);*/
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+void CMFCApplication3View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFCApplication3View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFCApplication3View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication3View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication3View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication3Doc* CMFCApplication3View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication3Doc)));
+	return (CMFCApplication3Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication3View 消息处理程序
